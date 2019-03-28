9790296f3d95a33fa4fe2f526456b8d2e29ed8e8
Rex Raphael <rex@snowball.com>
2019-03-28 19:13:39 +0100

    added example

---
+++ example/unity/DemoApp/Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 RotateAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RotateAmount = new Vector3(10, 10, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(RotateAmount * Time.deltaTime * 10);
+    }
+}
