d72ee8329ec95b7c54ed8eda57cb3f8c32c36e35
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-10-18 18:30:20 +0200

    feat(windows): added default windows files for example

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_unity_widget/flutter_unity_widget_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterUnityWidgetPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterUnityWidgetPlugin"));
+}
