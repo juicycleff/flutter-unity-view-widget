f67d0c00dac9ac3ab409784f952ec53c90d47d99
Rex Isaac Raphael <rex.raphael@outlook.com>
2022-05-30 17:52:05 +0200

    chore: remove commented out code

--- ios/Classes/FLTUnityView.swift
+++ ios/Classes/FLTUnityView.swift
@@ -10,34 +10,10 @@ import UIKit
 import UnityFramework
 
 class FLTUnityView: UIView {
-//    public var uView: UIView?
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-
-//    deinit {
-//    }
-
-//    func setUnityView(_ view: UIView?) {
-//        uView = view
-//        setNeedsLayout()
-//    }
-
     override func layoutSubviews() {
         super.layoutSubviews()
-//        uView?.removeFromSuperview()
-//        if let uView = uView {
-//            insertSubview(uView, at: 0)
-//        }
-//        (uView)?.frame = bounds
-//        uView?.setNeedsLayout()
         if (!self.bounds.isEmpty) {
             GetUnityPlayerUtils().ufw?.appController()?.rootView.frame = self.bounds
         }
     }
-
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//    }
 }
