e5f0d3b8ff04c6026394ff330d2ce4f94094d13d
Rebar Ahmad <rebar.ahmad@gmail.com>
2022-06-19 18:53:28 +0200

    Add Css for webview to behave like mobile

--- example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/Build.cs
+++ example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/Build.cs
@@ -249,12 +249,38 @@ namespace FlutterUnityIntegration.Editor
               });
             ");
 
-            indexHtmlText = indexHtmlText.Replace("}).then((unityInstance) => {", @"
+            indexHtmlText = indexHtmlText.Replace("canvas.style.width = \"960px\";", "canvas.style.width = \"100%\";");
+			indexHtmlText = indexHtmlText.Replace("canvas.style.height = \"600px\";", "canvas.style.height = \"100%\";");
+
+			indexHtmlText = indexHtmlText.Replace("}).then((unityInstance) => {", @"
          }).then((unityInstance) => {
            window.parent.postMessage('unityReady', '*');
            mainUnityInstance = unityInstance;
          ");
-            File.WriteAllText(indexFile, indexHtmlText);
+			File.WriteAllText(indexFile, indexHtmlText);
+
+			/// Modidy style.css
+			var cssFile = Path.Combine($"{WebExportPath}/TemplateData", "style.css");
+			var fullScreenCss = File.ReadAllText(cssFile);
+			fullScreenCss = @"
+                body { padding: 0; margin: 0; overflow: hidden; }
+                #unity-container { position: absolute }
+                #unity-container.unity-desktop { width: 100%; height: 100% }
+                #unity-container.unity-mobile { width: 100%; height: 100% }
+                #unity-canvas { background: #231F20 }
+                .unity-mobile #unity-canvas { width: 100%; height: 100% }
+                #unity-loading-bar { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); display: none }
+                #unity-logo { width: 154px; height: 130px; background: url('unity-logo-dark.png') no-repeat center }
+                #unity-progress-bar-empty { width: 141px; height: 18px; margin-top: 10px; background: url('progress-bar-empty-dark.png') no-repeat center }
+                #unity-progress-bar-full { width: 0%; height: 18px; margin-top: 10px; background: url('progress-bar-full-dark.png') no-repeat center }
+                #unity-footer { display: none }
+                .unity-mobile #unity-footer { display: none }
+                #unity-webgl-logo { float:left; width: 204px; height: 38px; background: url('webgl-logo.png') no-repeat center }
+                #unity-build-title { float: right; margin-right: 10px; line-height: 38px; font-family: arial; font-size: 18px }
+                #unity-fullscreen-button { float: right; width: 38px; height: 38px; background: url('fullscreen-button.png') no-repeat center }
+                #unity-mobile-warning { position: absolute; left: 50%; top: 5%; transform: translate(-50%); background: white; padding: 10px; display: none }
+            ";
+			File.WriteAllText(cssFile, fullScreenCss);
         }
 
         private static void ModifyAndroidGradle(bool isPlugin)
