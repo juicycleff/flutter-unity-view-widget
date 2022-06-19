04dc68dc610cb3007d7df211405bfdae2529313f
Rebar Ahmad <rebar.ahmad@gmail.com>
2022-06-19 23:26:12 +0200

    Fix char-space for web build

--- example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/Build.cs
+++ example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/Build.cs
@@ -223,37 +223,37 @@ namespace FlutterUnityIntegration.Editor
             var indexHtmlText = File.ReadAllText(indexFile);
 
             indexHtmlText = indexHtmlText.Replace("<script>", @"
-            <script>
-              var mainUnityInstance;
+    <script>
+        var mainUnityInstance;
 
-              window['handleUnityMessage'] = function (params) {
-                window.parent.postMessage({
-                    name: 'onUnityMessage',
-                    data: params,
-                   }, '*');
-              };
+        window['handleUnityMessage'] = function (params) {
+        window.parent.postMessage({
+            name: 'onUnityMessage',
+            data: params,
+            }, '*');
+        };
 
-              window['handleUnitySceneLoaded'] = function (name, buildIndex, isLoaded, isValid) {
-                window.parent.postMessage({
-                    name: 'onUnitySceneLoaded',
-                    data: {
-                        'name': name,
-                        'buildIndex': buildIndex,
-                        'isLoaded': isLoaded == 1,
-                        'isValid': isValid == 1,
-                    }
-                   }, '*');
-              };
+        window['handleUnitySceneLoaded'] = function (name, buildIndex, isLoaded, isValid) {
+        window.parent.postMessage({
+            name: 'onUnitySceneLoaded',
+            data: {
+                'name': name,
+                'buildIndex': buildIndex,
+                'isLoaded': isLoaded == 1,
+                'isValid': isValid == 1,
+            }
+            }, '*');
+        };
 
-              window.parent.addEventListener('unityFlutterBiding', function (args) {
-                const obj = JSON.parse(args.data);
-                mainUnityInstance.SendMessage(obj.gameObject, obj.methodName, obj.message);
-              });
+        window.parent.addEventListener('unityFlutterBiding', function (args) {
+            const obj = JSON.parse(args.data);
+            mainUnityInstance.SendMessage(obj.gameObject, obj.methodName, obj.message);
+        });
 
-              window.parent.addEventListener('unityFlutterBidingFnCal', function (args) {
-                mainUnityInstance.SendMessage('GameManager', 'HandleWebFnCall', args);
-              });
-            ");
+        window.parent.addEventListener('unityFlutterBidingFnCal', function (args) {
+            mainUnityInstance.SendMessage('GameManager', 'HandleWebFnCall', args);
+        });
+        ");
 
             indexHtmlText = indexHtmlText.Replace("canvas.style.width = \"960px\";", "canvas.style.width = \"100%\";");
 			indexHtmlText = indexHtmlText.Replace("canvas.style.height = \"600px\";", "canvas.style.height = \"100%\";");
@@ -269,22 +269,22 @@ namespace FlutterUnityIntegration.Editor
 			var cssFile = Path.Combine($"{WebExportPath}/TemplateData", "style.css");
 			var fullScreenCss = File.ReadAllText(cssFile);
 			fullScreenCss = @"
-                body { padding: 0; margin: 0; overflow: hidden; }
-                #unity-container { position: absolute }
-                #unity-container.unity-desktop { width: 100%; height: 100% }
-                #unity-container.unity-mobile { width: 100%; height: 100% }
-                #unity-canvas { background: #231F20 }
-                .unity-mobile #unity-canvas { width: 100%; height: 100% }
-                #unity-loading-bar { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); display: none }
-                #unity-logo { width: 154px; height: 130px; background: url('unity-logo-dark.png') no-repeat center }
-                #unity-progress-bar-empty { width: 141px; height: 18px; margin-top: 10px; background: url('progress-bar-empty-dark.png') no-repeat center }
-                #unity-progress-bar-full { width: 0%; height: 18px; margin-top: 10px; background: url('progress-bar-full-dark.png') no-repeat center }
-                #unity-footer { display: none }
-                .unity-mobile #unity-footer { display: none }
-                #unity-webgl-logo { float:left; width: 204px; height: 38px; background: url('webgl-logo.png') no-repeat center }
-                #unity-build-title { float: right; margin-right: 10px; line-height: 38px; font-family: arial; font-size: 18px }
-                #unity-fullscreen-button { float: right; width: 38px; height: 38px; background: url('fullscreen-button.png') no-repeat center }
-                #unity-mobile-warning { position: absolute; left: 50%; top: 5%; transform: translate(-50%); background: white; padding: 10px; display: none }
+body { padding: 0; margin: 0; overflow: hidden; }
+#unity-container { position: absolute }
+#unity-container.unity-desktop { width: 100%; height: 100% }
+#unity-container.unity-mobile { width: 100%; height: 100% }
+#unity-canvas { background: #231F20 }
+.unity-mobile #unity-canvas { width: 100%; height: 100% }
+#unity-loading-bar { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); display: none }
+#unity-logo { width: 154px; height: 130px; background: url('unity-logo-dark.png') no-repeat center }
+#unity-progress-bar-empty { width: 141px; height: 18px; margin-top: 10px; background: url('progress-bar-empty-dark.png') no-repeat center }
+#unity-progress-bar-full { width: 0%; height: 18px; margin-top: 10px; background: url('progress-bar-full-dark.png') no-repeat center }
+#unity-footer { display: none }
+.unity-mobile #unity-footer { display: none }
+#unity-webgl-logo { float:left; width: 204px; height: 38px; background: url('webgl-logo.png') no-repeat center }
+#unity-build-title { float: right; margin-right: 10px; line-height: 38px; font-family: arial; font-size: 18px }
+#unity-fullscreen-button { float: right; width: 38px; height: 38px; background: url('fullscreen-button.png') no-repeat center }
+#unity-mobile-warning { position: absolute; left: 50%; top: 5%; transform: translate(-50%); background: white; padding: 10px; display: none }
             ";
 			File.WriteAllText(cssFile, fullScreenCss);
         }
